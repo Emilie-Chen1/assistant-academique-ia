298670330965301e27720bee2d352876bbe42707
Saal Ahmed <saal.ahmed@hotmail.com>
2026-04-13 23:29:20 +0200

    ajout tool calculator avec numexpr + requirements maj

--- agents.py
+++ agents.py
@@ -1,3 +1,5 @@
+import numexpr
+
 from dotenv import load_dotenv, find_dotenv
 from langchain.agents import create_agent
 from langchain_core.tools import tool
@@ -7,6 +9,18 @@ load_dotenv(find_dotenv())
 
 _tavily = TavilyClient()
 
+@tool
+def calculator(expression: str) -> str:
+    """Calcule une expression mathématique simple."""
+    try:
+        value = numexpr.evaluate(expression.strip())
+        if hasattr(value, "item"):
+            value = value.item()  # convertit numpy scalar en float/int
+        if isinstance(value, float) and value.is_integer():
+            return str(int(value))
+        return str(value)
+    except Exception as e:
+        return f"Erreur calculatrice: {e}"
 
 @tool
 def web_search(query: str) -> str:
@@ -19,15 +33,16 @@ def web_search(query: str) -> str:
 
 
 AGENT_MODEL = "openai:gpt-4o-mini"
-AGENT_TOOLS = [web_search]
+AGENT_TOOLS = [calculator, web_search]
 
 _agent = create_agent(
     model=AGENT_MODEL,
     tools=AGENT_TOOLS,
     system_prompt=
         "Tu es un assistant utile. "
-        "Utilise web_search pour les recherches web. "
+        "Utilise calculator pour les calculs et web_search pour les recherches web. "
         "Réponds en français.",
+    # debug=True
 )
 
 
